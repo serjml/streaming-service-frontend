4c0ac8525cfcf3f06ef2980ceceac5b0b98e8b3b
Serj Lyakhov <serjml@list.ru>
2025-11-27 07:23:25 +0300

    feat: add hero section

--- src/sections/Hero/Hero.jsx
+++ src/sections/Hero/Hero.jsx
@@ -1,12 +1,14 @@
 import './Hero.scss'
+import Button from '@/components/Button'
 
 export default (props) => {
   const playButtonTitle = 'Play Video'
+  const titleId = 'hero-title'
 
   return (
     <section
       className="hero"
-      aria-labelledby="gero-title"
+      aria-labelledby={titleId}
     >
       <div className="hero__pano">
         <div className="hero__pano-inner container">
@@ -27,7 +29,22 @@ export default (props) => {
         </div>
       </div>
       <div className="hero__body">
-        <div className="hero__body-inner container"></div>
+        <div className="hero__body-inner container">
+          <h1
+            className="hero__title"
+            id={titleId}
+          >The Best Streaming Experience
+          </h1>
+          <div className="hero__description">
+            <p>StreamVibe is the best streaming experience for watching your favorite movies and shows on demand, anytime, anywhere. With StreamVibe, you can enjoy a wide variety of content, including the latest blockbusters, classic movies, popular TV shows, and more. You can also create your own watchlists, so you can easily find the content you want to watch.</p>
+          </div>
+          <Button
+            className="hero__button"
+            iconName="play"
+            hasFillIcon
+            label="Start Watching Now"
+          />
+        </div>
       </div>
     </section>
   )
