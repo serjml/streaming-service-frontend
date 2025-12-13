f2aefe259c981b0ab872f9c4b67ed7d4f4c625a7
Serj Lyakhov <serjml@list.ru>
2025-12-13 08:42:07 +0300

    feat: add category card component layout

--- src/components/CategoryCard/CategoryCard.jsx
+++ src/components/CategoryCard/CategoryCard.jsx
@@ -1,24 +1,32 @@
 import './CategoryCard.scss'
 import {Image} from 'minista'
+import Icon from '@/components/Icon'
 
 export default (props) => {
   const {
-    title, images = [],
+    title,
+    images = [],
   } = props
 
   return (<a
-      className="category-card"
-      href="/movies"
-    >
-      <div className="category-card__images">
-        {images.map((imgSrc, index) => (
-          <Image
-            className="category-card__image"
-            src={imgSrc}
-            key={index}
-          />
-        ))}
-      </div>
-      <div className="category-card__body"></div>
-    </a>)
+    className="category-card"
+    href="/movies"
+  >
+    <div className="category-card__images">
+      {images.map((imgSrc, index) => (
+        <Image
+          className="category-card__image"
+          src={imgSrc}
+          key={index}
+        />
+      ))}
+    </div>
+    <div className="category-card__body">
+      <h3 className="category-card__title">{title}</h3>
+      <Icon
+        className="category-card__icon"
+        name="arrow-right"
+      />
+    </div>
+  </a>)
 }
