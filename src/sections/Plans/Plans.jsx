c036420b376204ac1d755d8fc72c5c0bde476576
Serj Lyakhov <serjml@list.ru>
2025-12-25 06:39:50 +0300

    feat(js): add planGroup tabs info

--- src/sections/Plans/Plans.jsx
+++ src/sections/Plans/Plans.jsx
@@ -1,13 +1,19 @@
 import Section from '@/layouts/Section'
 import PlanCard from '@/components/PlanCard'
+import Grid from '@/components/Grid'
 
 export default () => {
+  // const planCardItems =
+
   return (
     <Section
       title="Choose the plan that's right for you"
       titleId="Plans-id"
       description="Join StreamVibe and select from our flexible subscription options tailored to suit your viewing preferences. Get ready for non-stop entertainment!"
     >
+      <Grid columns={3}>
+
+      </Grid>
       <PlanCard
         title="1"
         description="1"
