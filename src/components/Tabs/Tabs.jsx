e0c4ff5db3456ffd2e37f37d508059be8704c1cd
Serj Lyakhov <serjml@list.ru>
2025-12-26 05:59:41 +0300

    feat(jsx): add tabs coponent layout

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,16 +1,52 @@
 import './Tabs.scss'
 import clsx from 'clsx'
+import getIdFromTitle from '@/utils/getIdFromTitle'
 
 export default (props) => {
   const {
     className,
+    title,
+    items = [],
+    navigationTargetElementId = null,
   } = props
-  
+
   return (
     <div
       className={clsx('tabs', className)}
+      data-js-tabs={JSON.stringify({navigationTargetElementId})}
     >
-      Tabs
+      {!navigationTargetElementId && (
+        <div>TABS NAVIGATION</div>
+      )}
+      <div className="tabs__body">
+        {items.map((item, index) => {
+            const {
+              title,
+              children,
+              isActive
+            } = item
+
+            const titleFormatted = getIdFromTitle(title)
+            const buttonId = `${titleFormatted}-tab`
+            const contentId = `${titleFormatted}-tabpanel`
+
+            return (
+              <div
+                className={clsx('tabs__content', {
+                  'is-active': isActive,
+                })}
+                id={contentId}
+                aria-labelledby={buttonId}
+                tabIndex={0}
+                data-js-tabs-content=""
+                key={index}
+              >
+                {children}
+              </div>
+            )
+          }
+        )}
+      </div>
     </div>
   )
 }
