3b02f75233e7d0892d4ae39f205ac1edc950445c
Serj Lyakhov <serjml@list.ru>
2026-02-18 10:26:14 +0300

    feat(jsx): add MovieBanner section

--- src/pages/movie.jsx
+++ src/pages/movie.jsx
@@ -1,3 +1,5 @@
+import MoviesBanner from '@/sections/MoviesBanner'
+
 export const metadata = {
   title: 'Movie - Kantara'
 }
@@ -5,7 +7,7 @@ export const metadata = {
 export default () => {
   return (
     <>
-
+      <MoviesBanner />
     </>
   )
 }
---
+++ src/sections/MovieBanner/MovieBanner.jsx
@@ -0,0 +1,19 @@
+import './MovieBanner.scss'
+import MovieBannerCard from '@/components/MovieBannerCard'
+
+export default (props) => {
+  const titleId = 'movie-banner-title'
+
+  return (
+    <section
+      className="container"
+      aria-labelledby={titleId}
+    >
+      <MovieBannerCard
+        title="Kantara"
+        description="A fiery young man clashes with an unflinching forest officer in a south Indian village where spirituality, fate and folklore rule the lands."
+        imgSrc="/src/assets/images/movie-banner/2.jpg"
+      />
+    </section>
+  )
+}
