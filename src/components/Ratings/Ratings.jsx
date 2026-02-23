9d961f207caa7f6f5b374dd5f97ff363346ee2b2
Serj Lyakhov <serjml@list.ru>
2026-02-23 05:52:10 +0300

    feat(jsx): add Ratings component for MovieDetails aside

---
+++ src/components/Ratings/Ratings.jsx
@@ -0,0 +1,29 @@
+import './Ratings.scss'
+import RatingView from '@/components/RatingView'
+
+export default (props) => {
+  const {
+    items = [],
+  } = props
+
+  return (
+    <div
+      className="ratings"
+    >
+      <ul className="ratings__list">
+        {items.map(({title, ratingValue}, index) => (
+          <li
+            className="ratings__item"
+            key={index}
+          >
+            <h4 className="ratings__title">{title}</h4>
+            <RatingView
+              value={ratingValue}
+              label={ratingValue}
+            />
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
+}
