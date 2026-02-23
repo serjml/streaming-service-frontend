27e98b8ab61c0eabc547e75a33765955fd4ce355
Serj Lyakhov <serjml@list.ru>
2026-02-23 06:15:33 +0300

    feat(jsx/scss): modify PersonCard component(add new parameters) for MovieDetails aside

--- src/components/PersonCard/PersonCard.jsx
+++ src/components/PersonCard/PersonCard.jsx
@@ -5,7 +5,11 @@ export default (props) => {
   const {
     imgSrc,
     imgAlt,
+    name,
+    subtitle
   } = props
+
+  const hasBody = Boolean(name || subtitle)
 
   return (
     <div
@@ -17,6 +21,12 @@ export default (props) => {
         alt={imgAlt}
         title={imgAlt}
       />
+      {hasBody && (
+        <div className="person-card__body">
+          {name && <h4 className="person-card__name">{name}</h4>}
+          {subtitle && <h4 className="person-card__subtitle">{subtitle}</h4>}
+        </div>
+      )}
     </div>
   )
 }
