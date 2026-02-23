2494c05030601c805f487ac83fba57b5fb1290c6
Serj Lyakhov <serjml@list.ru>
2026-02-23 05:41:30 +0300

    feat(jsx): add Tags component for MovieDetails aside

---
+++ src/components/Tags/Tags.jsx
@@ -0,0 +1,24 @@
+import './Tags.scss'
+
+export default (props) => {
+  const {
+    items = []
+  } = props
+
+  return (
+    <div
+      className="tags"
+    >
+      <ul className="tags__list">
+        {items.map((tag, index) => (
+          <li
+            className="tags__item"
+            key={index}
+          >
+            {tag}
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
+}
