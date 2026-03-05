1ac79e6aea73ee968488f042aaa23d0c8dbc4896
Serj Lyakhov <serjml@list.ru>
2026-03-05 09:18:11 +0300

    feat(jsx): add EpisodeCard component layout

---
+++ src/components/EpisodeCard/EpisodeCard.jsx
@@ -0,0 +1,56 @@
+import './EpisodeCard.scss'
+import Icon from '@/components/Icon'
+
+export default (props) => {
+  const {
+    number,
+    title,
+    description,
+    duration,
+    video
+  } = props
+
+  const playButtonTitle = 'Play video'
+
+  return (
+    <div className="episode-card">
+      <div className="episode-card__number">
+        {number}
+      </div>
+      <div className="episode-card__player">
+        <video
+          className="episode-card__video"
+          src={video.src}
+          poster={video.poster}
+          width={172}
+          height={118}
+        />
+        <button
+          className="episode-card__play-button is-active"
+          aria-label={playButtonTitle}
+          title={playButtonTitle}
+        >
+          <Icon
+            className="episode-card__play-button-icon"
+            name="play-circle"
+          />
+        </button>
+      </div>
+      <div className="episode-card__body">
+        <div className="episode-card__info">
+          <h4 className="episode-card__title h6">{title}</h4>
+          <div className="episode-card__duration">
+            <Icon
+              name="clock_stroke"
+              ariaLabel="Durasion"
+            />
+            <span>{duration}</span>
+          </div>
+        </div>
+        <div className="episode-card__description hidden-mobile">
+          <p>{description}</p>
+        </div>
+      </div>
+    </div>
+  )
+}
