1ab701e19c7bb0364f0964cd570e4e568461ce0d
Serj Lyakhov <serjml@list.ru>
2026-03-06 07:03:06 +0300

    feat(scss): add EpisodeCard component styles

--- src/components/Seasons/Seasons.jsx
+++ src/components/Seasons/Seasons.jsx
@@ -2,6 +2,7 @@ import './Seasons.scss'
 import AccordionGroup from '@/components/AccordionGroup'
 import seasonsitems from '@/components/Seasons/seasonsitems'
 import Accordion from '@/components/Accordion'
+import EpisodeCard from '@/components/EpisodeCard'
 
 export default () => {
 
@@ -29,7 +30,13 @@ export default () => {
                   className="seasons__items"
                   key={index}
                 >
-                  {episode.title}
+                  <EpisodeCard
+                    number={episode.number}
+                    title={episode.title}
+                    description={episode.description}
+                    duration={episode.duration}
+                    video={episode.video}
+                  />
                 </li>
               ))}
             </ul>
