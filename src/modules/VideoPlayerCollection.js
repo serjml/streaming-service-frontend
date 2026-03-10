c4d449b2cd58d44a466485811acd8c10af1d07a4
Serj Lyakhov <serjml@list.ru>
2026-03-10 11:10:54 +0300

    feat(js): add selectors & stateClasses & constructor & bindEvent methods to VideoPlyer Class

--- src/modules/VideoPlayerCollection.js
+++ src/modules/VideoPlayerCollection.js
@@ -1,6 +1,26 @@
 const rootSelector = '[data-js-video-player]'
 
 class VideoPlayer {
+  selectors = {
+    root: rootSelector,
+    video: '[data-js-video-player-video]',
+    playButton: '[data-js-video-player-play-button]',
+  }
+
+  stateClasses = {
+    isActive: 'is-active',
+  }
+
+  constructor(rootElement) {
+    this.rootElement = rootElement
+    this.videoElement = this.rootElement.querySelector(this.selectors.video)
+    this.playButtonElement = this.rootElement.querySelector(this.selectors.playButton)
+  }
+
+  bindEvents() {
+    this.playButtonElement.addEventListener('click', this.onPlayButtonClick)
+    this.videoElement.addEventListener('pause', this.onVideoPause)
+  }
 }
 
 class VideoPlayerCollection {
