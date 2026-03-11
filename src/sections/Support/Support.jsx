cb12504a9de1c8df8dbc16a8d5ad6e3c16fdec0b
Serj Lyakhov <serjml@list.ru>
2026-03-11 07:03:55 +0300

    feat(jsx): add Support component without form

--- src/sections/Support/Support.jsx
+++ src/sections/Support/Support.jsx
@@ -1,16 +1,37 @@
 import './Support.scss'
-import clsx from 'clsx'
+import {Image} from 'minista'
 
 export default (props) => {
-  const {
-    className,
-  } = props
+  const titleId = 'support-title'
 
   return (
-    <div
-      className={clsx('support', className)}
+    <section
+      className="support container"
+      aria-labelledby={titleId}
     >
-      Support
-    </div>
+      <div className="support__body">
+        <div className="support__info">
+          <h1
+            className="support__title h2"
+            id={titleId}
+          >
+            Welcome to our support page!
+          </h1>
+          <div className="support__description">
+            <p>
+              We're here to help you with any problems you may be having with our product.
+            </p>
+          </div>
+        </div>
+        <Image
+          className="support__image"
+          src="/src/assets/images/support/1.png"
+        />
+      </div>
+      <form
+        className="support__form"
+        action=""
+      ></form>
+    </section>
   )
 }
