584cb63bf6d2069396e9447ed18999eff1edd7a2
Serj Lyakhov <serjml@list.ru>
2026-03-11 07:21:03 +0300

    feat(jsx): add Field component basic layout

---
+++ src/components/Field/Field.jsx
@@ -0,0 +1,46 @@
+import './Field.scss'
+import clsx from 'clsx'
+import getIdFromTitle from '@/utils/getIdFromTitle'
+
+export default (props) => {
+  const {
+    className,
+    id = getIdFromTitle(props.label),
+    label,
+    /**
+     * undefined (default) | 'email' | 'textarea'
+     **/
+    type,
+    placeholder,
+    isRequired,
+  } = props
+
+  const Component = type === 'textarea' ? 'textarea' : 'input'
+
+  return (
+    <div
+      className={clsx('field', className)}
+    >
+      <label
+        className="field__label"
+        htmlFor={id}
+      >
+        {label} {isRequired && (
+        <span
+          className="field__required-star"
+          aria-hidden={true}
+        >*</span>
+      )}
+      </label>
+      <div className="field__body">
+        <Component
+          className="field__control"
+          id={id}
+          type={type}
+          palceholder={placeholder}
+          required={isRequired}
+        />
+      </div>
+    </div>
+  )
+}
