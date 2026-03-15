0d4bde45d0220ade314f662cac20291566b361e5
Serj Lyakhov <serjml@list.ru>
2026-03-15 08:30:35 +0300

    feat(js): implement InputMask class & add mask functional to input tel

--- src/modules/InputMaskCollection.js
+++ src/modules/InputMaskCollection.js
@@ -1,6 +1,25 @@
+import getAttrNameFromSelector from '@/utils/getAttrNameFromSelector'
+import IMask from 'imask'
+
 const rootSelector = '[data-js-input-mask]'
 
 class InputMask {
+  selectors = {
+    root: rootSelector,
+  }
+
+  constructor(rootElement) {
+    this.rootElement = rootElement
+    this.init()
+  }
+
+  init() {
+    const mask = this.rootElement.getAttribute(
+      getAttrNameFromSelector(this.selectors.root)
+    )
+
+    IMask(this.rootElement, {mask})
+  }
 }
 
 class InputMaskCollection {
