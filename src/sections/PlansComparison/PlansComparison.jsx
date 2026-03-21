dd395dd0fcf44ddd8bea513a6bca8f3dfde006e7
Serj Lyakhov <serjml@list.ru>
2026-03-21 09:07:05 +0300

    feat(jsx): add Table component layout

---
+++ src/sections/PlansComparison/PlansComparison.jsx
@@ -0,0 +1,14 @@
+import './PlansComparison.scss'
+import Section from '@/layouts/Section'
+
+export default () => {
+  return (
+    <Section
+      title="Compare our plans and find the right one for you"
+      titleId="plans-comparison-title"
+      description="StreamVibe offers three different plans to fit your needs: Basic, Standard, and Premium. Compare the features of each plan and choose the one that's right for you."
+    >
+      PlansComparison
+    </Section>
+  )
+}
