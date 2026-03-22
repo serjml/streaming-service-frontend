8cbb16833da3abd0ae693c99977936df9e0d70cf
Serj Lyakhov <serjml@list.ru>
2026-03-22 11:41:28 +0300

    feat(jsx): inject Tabs Specifications to plansComparison

--- src/sections/PlansComparison/PlansComparison.jsx
+++ src/sections/PlansComparison/PlansComparison.jsx
@@ -2,6 +2,8 @@ import './PlansComparison.scss'
 import Section from '@/layouts/Section'
 import Badge from '@/components/Badge'
 import Table from '@/components/Table'
+import Specifications from '@/components/Specifications'
+import Tabs from '@/components/Tabs'
 
 export default () => {
   const headCells = [
@@ -12,14 +14,17 @@ export default () => {
     {
       children: 'Basic',
       width: '25%',
+      tabsTitle: 'Basic',
     },
     {
       children: <>Standart <Badge mode="accent">Popular</Badge></>,
       width: '25%',
+      tabsTitle: 'Standart',
     },
     {
       children: 'Premium',
       width: '25%',
+      tabsTitle: 'Premium',
     },
   ]
 
@@ -108,6 +113,20 @@ export default () => {
     },
   ]
 
+  const tabsItems = headCells.filter((headCell) => headCell.tabsTitle).map((headCell, headCellIndex) => ({
+    title: headCell.tabsTitle,
+    isActive: headCellIndex === 0,
+    children: (
+      <Specifications
+        items={rows.map(({cells, isWide}) => ({
+          key: cells[0],
+          value: cells[headCellIndex + 1],
+          isWide,
+        }))}
+      />
+    )
+  }))
+
   return (
     <Section
       title="Compare our plans and find the right one for you"
@@ -115,8 +134,14 @@ export default () => {
       description="StreamVibe offers three different plans to fit your needs: Basic, Standard, and Premium. Compare the features of each plan and choose the one that's right for you."
     >
       <Table
+        className="hidden-mobile"
         headCells={headCells}
         rows={rows}
+      />
+      <Tabs
+        className="visible-mobile"
+        title="plans-comparison-tabs-title"
+        items={tabsItems}
       />
     </Section>
   )
