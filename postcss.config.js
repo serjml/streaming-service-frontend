1b0a70653437609227647f9100e887428ddfcfd7
Serj Lyakhov <serjml@list.ru>
2026-03-23 10:57:55 +0300

    chore: add postcss preset env

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,7 @@
 import postcssPxToRem from 'postcss-pxtorem'
+import postcssPresetEnv from 'postcss-preset-env'
 
-export default ({ env }) => {
+export default ({env}) => {
   const isProd = env === 'production'
   const plugins = []
 
@@ -10,6 +11,10 @@ export default ({ env }) => {
         propList: ['*'],
         mediaQuery: true,
       })
+    )
+
+    plugins.push(
+      postcssPresetEnv()
     )
   }
 
